819ea9b2f759d2f33aca4b44d914ef6a3bb8579d
Patrykbartczak <patryk.bartczak99@gmail.com>
2025-11-26 19:28:16 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+from idlelib.query import Query
+
+import dotenv
+import os
+dotenv.load_dotenv()
+
+class Config:
+    QUERY = os.getenv('QUERY')
+    API_KEY = os.getenv('API_KEY')
+    XLSX_PATH = 'pogoda.xlsx'
---
+++ tools.py
@@ -0,0 +1,7 @@
+
+
+def conver_to_celsius(value):
+    return round(value - 273.15,2)
+
+def ms_to_kmh(speed_ms):
+    return speed_ms * 3.6
